987fe985c19cdbb3de670686ac1483f50a515e1c
damianfral <huevofritopamojarpan@gmail.com>
2023-02-11 16:04:28 +0000

    Add flake.

---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+  description = "A tool to transfer color between images.";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+    nix-filter.url = "github:numtide/nix-filter";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, nix-filter, ... }:
+
+    let
+      pkgsFor = system: import nixpkgs {
+        inherit system;
+        overlays = [
+          self.overlays.${system}
+          nix-filter.overlays.default
+        ];
+      };
+    in
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = pkgsFor system;
+        filteredSrc =
+          pkgs.nix-filter {
+            root = ./.;
+            include = [
+              "src/"
+              "test/"
+              "package.yaml"
+              "LICENSE"
+            ];
+          };
+      in
+      rec {
+        packages = {
+          colortransfer = pkgs.haskell.lib.justStaticExecutables (
+            pkgs.haskellPackages.colortransfer
+          );
+        };
+
+        defaultPackage = packages.colortransfer;
+
+        devShells.default = pkgs.haskellPackages.shellFor {
+          packages = p: [ packages.colortransfer ];
+          buildInputs = with pkgs; with pkgs.haskellPackages; [
+            haskell-language-server
+            cabal-install
+            ghcid
+            hpack
+            hlint
+            yamlfix
+          ];
+        };
+
+        overlays = final: prev: with final.haskell.lib; {
+          haskellPackages = prev.haskellPackages.override (old: {
+            overrides = final.lib.composeExtensions (old.overrides or (_: _: { }))
+              (self: super: {
+                colortransfer = self.generateOptparseApplicativeCompletions
+                  [ "colortransfer" ]
+                  (self.callCabal2nix "colortransfer" filteredSrc { });
+              }
+              );
+          });
+        };
+      });
+}
+
