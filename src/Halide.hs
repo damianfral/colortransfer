7a29b048cd2cd17d7108804cba22036b40f12f89
damianfral <huevofritopamojarpan@gmail.com>
2023-03-11 04:23:45 +0000

    Simplification.

--- src/Halide.hs
+++ src/Halide.hs
@@ -28,11 +28,14 @@ import Data.Data (Proxy (..))
 import Data.Function (on)
 import Data.Generics.Fixplate
 import Data.Generics.Labels ()
+import Data.Generics.Product ()
 import Data.String (IsString)
 import Data.Text (Text, intercalate, pack)
+import GHC.Generics (Generic)
 
 --------------------------------------------------------------------------------
 
+-- | names to use as variables in the definition of a Func.
 newtype Var = Var {unVar :: Text}
   deriving stock (Eq, Show)
   deriving newtype (IsString)
@@ -47,10 +50,14 @@ newtype Param (a :: ScalarType) = Param Text
 newtype ImageParam (d :: Dimensions) (a :: ScalarType) = ImageParam Text
   deriving (Show)
 
+-- | A Func represents a pipeline stage.
 data Func (d :: Dimensions) (a :: ScalarType) = Func
   { name :: FuncName,
     body :: InferArgs d -> Expr a
   }
+
+--- | A wrapper over `Func d a` to hide the type variables.
+data Func' = forall d a. Func' (Func d a)
 
 -- All Exprs have a scalar type, and all Funcs evaluate to one or
 -- more scalar types. The scalar types in Halide are unsigned
@@ -115,6 +122,7 @@ type family InferArgs a where
   InferArgs 'D3 = N3 Var
   InferArgs 'D4 = N4 Var
 
+-- | @Expr's are complex expressions using @Vars.
 data Expr (a :: ScalarType) where
   EVar :: Var -> Expr 'U32
   EInt :: Int -> Expr 'U64
@@ -134,17 +142,13 @@ instance Num (Expr (a :: ScalarType)) where
 
 data N0 a = N0
 
-newtype N1 a = N1 a
-  deriving (Functor)
+newtype N1 a = N1 a deriving (Functor)
 
-data N2 a = N2 a a
-  deriving (Functor)
+data N2 a = N2 a a deriving (Functor)
 
-data N3 a = N3 a a a
-  deriving (Functor)
+data N3 a = N3 a a a deriving (Functor)
 
-data N4 a = N4 a a a a
-  deriving (Functor)
+data N4 a = N4 a a a a deriving (Functor)
 
 class HasCoordinates d where
   type Coordinates d
@@ -172,16 +176,15 @@ instance HasCoordinates 'D4 where
 
 --------------------------------------------------------------------------------
 
-data At d a = forall f. Space d f => At {space :: f d a, coordinates :: Coordinates d}
+data At d a = forall f.
+  Space d f =>
+  At
+  { space :: f d a,
+    coordinates :: Coordinates d,
+    func :: Maybe (Func d a)
+  }
 
-data AtSimple = AtSimple {atSpaceID :: Text, atCoordinates :: [Expr 'U32]}
-
-atToAtSimple :: forall d a. (HasCoordinates d) => At d a -> AtSimple
-atToAtSimple (At space coordinates) =
-  AtSimple
-    { atSpaceID = spaceID space,
-      atCoordinates = coordinatesToList @d coordinates
-    }
+data At' = forall d a. At' (At d a)
 
 data ExprF f
   = FVar Var
@@ -192,14 +195,15 @@ data ExprF f
   | FMul f f
   | FMin f f
   | FMax f f
-  | FAt Text [f]
+  | FAt At' [f]
   | FCast CastSimple f
   deriving (Functor)
 
 data Tag = Tag
   { vars :: [Var],
     params :: [Text],
-    spaces :: [Text]
+    spaces :: [Text],
+    funcs :: [Func']
   }
 
 instance Semigroup Tag where
@@ -207,11 +211,11 @@ instance Semigroup Tag where
     Tag
       { vars = ((<>) `on` vars) t1 t2,
         params = ((<>) `on` params) t1 t2,
-        spaces = ((<>) `on` spaces) t1 t2
+        spaces = ((<>) `on` spaces) t1 t2,
+        funcs = ((<>) `on` funcs) t1 t2
       }
 
-instance Monoid Tag where
-  mempty = Tag [] [] []
+instance Monoid Tag where mempty = Tag [] [] [] []
 
 type MuExprF = Mu ExprF
 
@@ -232,18 +236,21 @@ toMuExprF (ESub x y) = Fix $ FSub (toMuExprF x) (toMuExprF y)
 toMuExprF (EMul x y) = Fix $ FMul (toMuExprF x) (toMuExprF y)
 toMuExprF (EMin x y) = Fix $ FMin (toMuExprF x) (toMuExprF y)
 toMuExprF (EMax x y) = Fix $ FMax (toMuExprF x) (toMuExprF y)
-toMuExprF (EAt at) = atToFAt at
+toMuExprF (EAt a) = atToFAt a
   where
     atToFAt :: forall d a. At d a -> MuExprF
-    atToFAt (At {..}) =
-      Fix $ FAt (spaceID space) $ toMuExprF <$> coordinatesToList @d coordinates
+    atToFAt b@(At {..}) =
+      Fix $ FAt (At' b) $ toMuExprF <$> coordinatesToList @d coordinates
 toMuExprF (ECast cast x) = Fix $ FCast (castToCastSimple cast) (toMuExprF x)
 
 toAST :: Expr a -> Attr ExprF Tag
 toAST = synthetise f . toMuExprF
   where
     f (FVar v) = mempty {vars = [v]}
-    f (FAt space _) = mempty {spaces = [space]}
+    f (FAt (At' a) _) =
+      case func a of
+        Just g -> mempty {funcs = [Func' g]}
+        Nothing -> mempty -- {spaces = spaceID $ space at}
     f _ = mempty
 
 -- | An F-Algebra like explained
@@ -273,7 +280,7 @@ compileAST = cata (go . unAnn)
     go (FMul f g) = wrapInParens f <> " * " <> wrapInParens g
     go (FMin f g) = mconcat ["min", wrapInParens $ f <> ", " <> g]
     go (FMax f g) = mconcat ["max", wrapInParens $ f <> ", " <> g]
-    go (FAt spaceName g) = spaceName <> wrapInParens (intercalate ", " g)
+    go (FAt (At' (At {space})) g) = spaceID space <> wrapInParens (intercalate ", " g)
     go (FCast c g) = "<" <> c <> ">" <> wrapInParens g
 
 compile :: Expr a -> Text
@@ -285,7 +292,7 @@ testExpr :: Expr 'U32
 testExpr =
   let x = Var "x"
       y = Var "y"
-   in ECast u32 (EInt 2) * EAt (At gradient (N2 (EVar x) (EVar y)))
+   in ECast u32 (EInt 2) * EAt (At gradient (N2 (EVar x) (EVar y)) (Just gradient))
 
 data SpaceType = Function | Image deriving (Eq)
 
@@ -295,36 +302,38 @@ isFunc s = spaceType s == Function
 class (HasCoordinates d) => Space d f where
   spaceID :: f d a -> Text
   spaceType :: f d a -> SpaceType
+  at :: f d a -> Coordinates d -> At d a
 
 instance Space 'D1 Func where
   spaceID = unFuncName . name
   spaceType _ = Function
+  at f c = At {space = f, coordinates = c, func = Just f}
 
 instance Space 'D2 Func where
   spaceID = unFuncName . name
   spaceType _ = Function
+  at f c = At {space = f, coordinates = c, func = Just f}
 
 instance Space 'D3 Func where
   spaceID = unFuncName . name
   spaceType _ = Function
+  at f c = At {space = f, coordinates = c, func = Just f}
 
 instance Space 'D4 Func where
   spaceID = unFuncName . name
   spaceType _ = Function
+  at f c = At {space = f, coordinates = c, func = Just f}
 
 --------------------------------------------------------------------------------
 
-data Store = Store
-  { counter :: Int,
-    funcs :: Func (forall d. d :: Dimensions) (forall a. a)
-  }
+newtype Store = Store {counter :: Int} deriving (Generic)
 
 type AppM a = State Store a
 
 getNextID :: Text -> AppM Text
 getNextID prefix = do
+  modify $ #counter %~ (+) 1
   i <- gets counter
-  modify $ counterL %~ (+) 1
   pure $ prefix <> "_" <> pack (show i)
 
 counterL :: Lens' Store Int
@@ -333,22 +342,22 @@ counterL = lens counter (\s c -> s {counter = c})
 newVar :: AppM Var
 newVar = Var <$> getNextID "var_"
 
-newFunc :: (InferArgs d -> Expr a) -> AppM (Func d a)
+newFunc :: forall d a. (InferArgs d -> Expr a) -> AppM (Func d a)
 newFunc body = do
   name <- FuncName <$> getNextID "func_"
-  pure $ Func {name, body}
+  let func :: Func d a = Func {name, body}
+  pure func
 
 --------------------------------------------------------------------------------
 
 constant :: Func d 'U64
 constant = Func {name = "blur_x", body = \_ -> EInt 9}
 
--- brighten :: Space 'D2 f => Double -> f 'D2 a -> Func 'D2 ('F64 :: ScalarType)
 brighten :: (Space 'D2 f) => Double -> f 'D2 (a :: ScalarType) -> Func 'D2 'F64
 brighten q space = Func {name = "brighten", body}
   where
     body (N2 x y) =
-      EMul (EDouble q) $ EAt $ At space $ N2 (ECast u32 $ EVar x) (ECast u32 $ EVar y)
+      EMul (EDouble q) $ EAt $ At space (N2 (ECast u32 $ EVar x) (ECast u32 $ EVar y)) Nothing
 
 gradient :: Func 'D2 'U32
 gradient = Func {name = "gradient", body}
@@ -364,77 +373,20 @@ class Halogenide m a where
 class ExprType x where
   showType :: x -> Text
 
-instance ExprType (Proxy 'U8) where
-  showType _ = "UInt(8)"
+instance ExprType (Proxy 'U8) where showType _ = "UInt(8)"
 
-instance ExprType (Proxy 'U16) where
-  showType _ = "UInt(16)"
+instance ExprType (Proxy 'U16) where showType _ = "UInt(16)"
 
-instance ExprType (Proxy 'U32) where
-  showType _ = "UInt(32)"
+instance ExprType (Proxy 'U32) where showType _ = "UInt(32)"
 
-instance ExprType (Proxy 'U64) where
-  showType _ = "UInt(64)"
+instance ExprType (Proxy 'U64) where showType _ = "UInt(64)"
 
-instance ExprType (Proxy 'F32) where
-  showType _ = "Float(32)"
+instance ExprType (Proxy 'F32) where showType _ = "Float(32)"
 
-instance ExprType (Proxy 'F64) where
-  showType _ = "Float(64)"
-
--- -- instance Halogenide Identity (Space (d :: Dimensions) f) where
--- --   render = f d a -> Text
---
--- instance Halogenide Identity (Expr (a :: ScalarType)) where
---   render (EVar (Var v)) = pure v
---   render (EInt i) = pure $ pack $ show i
---   render (EDouble d) = pure $ pack $ show d
---   render (EAdd e1 e2) = pure $ wrapInParens $ runIdentity $ render e1 <> " + " <> render e2
---   render (ESub e1 e2) = pure $ wrapInParens $ runIdentity $ render e1 <> " - " <> render e2
---   render (EMul e1 e2) = pure $ wrapInParens $ runIdentity $ render e1 <> " * " <> render e2
---   render (EMin e1 e2) = pure $ "min(" <> runIdentity (render e1) <> ", " <> runIdentity (render e2) <> ")"
---   render (EMax e1 e2) = "max(" <> render e1 <> ", " <> render e2 <> ")"
---   render (EAt (At f pos)) = pure $ spaceID f <> "(" <> intercalate ", " render (coordinatesToList pos)
---   render (ECast (_, tType) e) = pure $ "cast(" <> tType <> "," <> runIdentity (render e) <> ")"
+instance ExprType (Proxy 'F64) where showType _ = "Float(64)"
 
 gradientFunc :: Func 'D2 'U32
 gradientFunc = Func {name = "gradient", body}
   where
     body (N2 x y) = ECast u32 $ (EAdd `on` EVar) x y
 
--- chatpgt, this is the piece that needs some more code in order to compile.
--- testProgram :: Program
--- testProgram = do
---   gradient <- newFunc $ \(x, y) -> ECast u8 $ (EAdd `on` EVar) x y
---   output <- newOutput "filename.jpeg"
---   realize gradient output
-
---
--- {-
--- Func blur_3x3(Func input) {
---   Func blur_x, blur_y;
---   Var x, y, xi, yi;
---
---   // The algorithm - no storage or order
---   blur_x(x, y) = (input(x-1, y) + input(x, y) + input(x+1, y))/3;
---   blur_y(x, y) = (blur_x(x, y-1) + blur_x(x, y) + blur_x(x, y+1))/3;
---
---   // The schedule - defines order, locality; implies storage
---   blur_y.tile(x, y, xi, yi, 256, 32)
---         .vectorize(xi, 8).parallel(y);
---   blur_x.compute_at(blur_y, x).vectorize(x, 8);
---
---   return blur_y;
--- }
---
--- let exprx =
--- blurx <- newFunc exprx
--- blury <- newFunc expry
---
-
--- do
-
---
--- -}
---
---
