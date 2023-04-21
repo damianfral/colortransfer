39ab147bdb5696dc7dfc79e0ec3be8a989ef1372
damianfral <huevofritopamojarpan@gmail.com>
2023-04-21 17:45:28 +0000

    Clean.

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
           ];
         };
 
-        overlays = final: prev: with final.haskell.lib; {
+        overlays = final: prev: {
           haskellPackages = prev.haskellPackages.override (old: {
             overrides = final.lib.composeExtensions (old.overrides or (_: _: { }))
               (self: super: {
