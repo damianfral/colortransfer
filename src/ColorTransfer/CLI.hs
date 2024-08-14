28d295233600a47db792b5ca6e12064ad345e325
damianfral <huevofritopamojarpan@gmail.com>
2024-08-14 00:43:12 +0000

    Add module ColorTransfer.CLI .
    Allow choosing between ellipsoid transformation and histogram matching from CLI.

---
+++ src/ColorTransfer/CLI.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeOperators #-}
+
+module ColorTransfer.CLI where
+
+import qualified Codec.Picture as P
+import qualified ColorTransfer.EllipsoidTransformation as ET
+import qualified ColorTransfer.HistogramMatching as HM
+import ColorTransfer.Utils
+import Options.Generic
+
+data Opts w = Opts
+  { input :: w ::: String <?> "the image to be used as the color source",
+    reference :: w ::: String <?> "the image we want to transform",
+    output :: w ::: String <?> "the output image",
+    histogram :: w ::: Bool <?> "use simple histogram matching"
+  }
+  deriving (Generic)
+
+instance ParseRecord (Opts Wrapped)
+
+run :: Opts Unwrapped -> IO ()
+run (Opts {..}) = do
+  inputImg <- P.readImage input
+  referenceImg <- P.readImage reference
+
+  let result =
+        (if histogram then HM.transferColor else ET.transferColor)
+          <$> (dynamicImagetoYCbCr <$> inputImg)
+          <*> (dynamicImagetoYCbCr <$> referenceImg)
+
+  case result of
+    Right img -> P.writePng output $ imageToRGB img
+    Left err -> putStrLn err
--- src/Main.hs
+++ src/Main.hs
@@ -1,52 +1,11 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TypeOperators #-}
 
 module Main where
 
-import Codec.Picture qualified as P
-import Codec.Picture.Saving qualified as P
-import Codec.Picture.Types
-import Codec.Picture.Types qualified as P
-import ColorTransfer.EllipsoidTransformation qualified as ET
-import ColorTransfer.HistogramMatching qualified as HM
-import Control.Applicative
-import Data.ByteString.Lazy.Char8 qualified as B
-import Data.Either
-import Data.Word
-import GHC.Generics (Generic)
+import qualified ColorTransfer.CLI as CLI
 import Options.Generic
-import System.Environment
-
-data Opts w = Opts
-  { source :: w ::: String <?> "the image to be used as the color source",
-    target :: w ::: String <?> "the image we want to transform",
-    output :: w ::: String <?> "the output image",
-    histogram :: w ::: Bool <?> "use simple histogram matching"
-  }
-  deriving (Generic)
-
-instance ParseRecord (Opts Wrapped)
-
-run :: Opts Unwrapped -> IO ()
-run (Opts {..}) = do
-  sourceImg <- P.readImage source
-  targetImg <- P.readImage target
-
-  let result =
-        (if histogram then HM.transferColor else ET.transferColor)
-          <$> (HM.dynamicImagetoYCbCr <$> sourceImg)
-          <*> (HM.dynamicImagetoYCbCr <$> targetImg)
-
-  case result of
-    Right img -> P.writePng output $ HM.imageToRGB img
-    Left err -> putStrLn err
 
 main :: IO ()
 main = do
   options <- unwrapRecord "Transfer color between images"
-  run options
+  CLI.run options
