5532082840c28d5552746e1af3737cf43f2f3eae
damianfral <huevofritopamojarpan@gmail.com>
2024-08-14 00:32:55 +0000

    Refactor HistogramMatching to use hmatrix.

--- src/ColorTransfer/HistogramMatching.hs
+++ src/ColorTransfer/HistogramMatching.hs
@@ -4,83 +4,29 @@
 module ColorTransfer.HistogramMatching where
 
 import qualified Codec.Picture as P
-import qualified Codec.Picture.Types as P
-import Relude
+import ColorTransfer.Utils
+import qualified Numeric.LinearAlgebra as L
+import Relude hiding (fromList, toList)
 
-type Triplet a = (a, a, a)
-
-sumPixels :: P.Image P.PixelYCbCr8 -> Triplet Double
-sumPixels = P.pixelFold fn (0.0, 0.0, 0.0)
-  where
-    fn :: Triplet Double -> Int -> Int -> P.PixelYCbCr8 -> Triplet Double
-    fn (a', b', c') _ _ ((P.PixelYCbCr8 a b c)) = (aa, bb, cc)
-      where
-        f a'' b'' = fromIntegral a'' + b''
-        !aa = f a a'
-        !bb = f b b'
-        !cc = f c c'
-
-meanPixels :: P.Image P.PixelYCbCr8 -> Triplet Double
-meanPixels img = fn $ sumPixels img
-  where
-    size = fromIntegral $ P.imageWidth img * P.imageHeight img
-    fn (x, y, z) = (x / size, y / size, z / size)
-
-stdPixels :: P.Image P.PixelYCbCr8 -> Triplet Double
-stdPixels img = g $ P.pixelFold fn (0.0, 0.0, 0.0) img
-  where
-    size = fromIntegral $ P.imageWidth img * P.imageHeight img
-    (ma, mb, mc) = meanPixels img
-    g (a, b, c) = (sqrt (a / size), sqrt (b / size), sqrt (c / size))
-    fn ::
-      Triplet Double ->
-      Int ->
-      Int ->
-      P.PixelYCbCr8 ->
-      Triplet Double
-    fn (a', b', c') _ _ ((P.PixelYCbCr8 a b c)) = (aa, bb, cc)
-      where
-        f a'' b'' = (fromIntegral a'' - b'') ** 2
-        !aa = a' + f a ma
-        !bb = b' + f b mb
-        !cc = c' + f c mc
-
+-- Perform color transfer using histogram matching
 transferColor :: P.Image P.PixelYCbCr8 -> P.Image P.PixelYCbCr8 -> P.Image P.PixelYCbCr8
-transferColor from to = P.pixelMap (transfer srcMean srcStd tarMean tarStd) to
+transferColor input reference = P.pixelMap (transfer srcMean srcStd tarMean tarStd) input
   where
-    (!srcMean, !srcStd) = (meanPixels from, stdPixels from)
-    (!tarMean, !tarStd) = (meanPixels to, stdPixels to)
+    !srcMean = meanPixels input
+    !srcStd = stdPixels input
+    !tarMean = meanPixels reference
+    !tarStd = stdPixels reference
 
-{-# INLINE zip5Triplets #-}
-zip5Triplets ::
-  (a -> a -> a -> a -> a -> b) ->
-  (a, a, a) ->
-  (a, a, a) ->
-  (a, a, a) ->
-  (a, a, a) ->
-  (a, a, a) ->
-  (b, b, b)
-zip5Triplets fn (a, b, c) (d, e, f) (g, h, i) (j, k, l) (m, n, o) =
-  (fn a d g j m, fn b e h k n, fn c f i l o)
-
-{-# INLINE clampToWord8 #-}
-clampToWord8 :: Double -> Word8
-clampToWord8 a
-  | a > 255 = 255
-  | a < 0 = 0
-  | otherwise = round a
-
+-- Apply the transfer function to a single pixel
 transfer ::
-  Triplet Double ->
-  Triplet Double ->
-  Triplet Double ->
-  Triplet Double ->
+  L.Vector Double -> -- srcMean
+  L.Vector Double -> -- srcStd
+  L.Vector Double -> -- tarMean
+  L.Vector Double -> -- tarStd
   P.PixelYCbCr8 ->
   P.PixelYCbCr8
-transfer srcMean srcStd tarMean tarStd (P.PixelYCbCr8 a b c) =
-  P.PixelYCbCr8 a' b' c'
-  where
-    f :: Double -> Double -> Double -> Double -> Double -> Word8
-    f m s m' s' p = clampToWord8 (s' / s * (p - m') + m)
-    tmp = (fromIntegral a, fromIntegral b, fromIntegral c) :: Triplet Double
-    (a', b', c') = zip5Triplets f srcMean srcStd tarMean tarStd tmp
+transfer srcMean srcStd tarMean tarStd pixel =
+  let pixelVec = pixelToVector pixel
+      transformed = (tarStd / srcStd) * (pixelVec - tarMean) + srcMean
+      [y', cb', cr'] = map clampToWord8 (L.toList transformed)
+   in P.PixelYCbCr8 y' cb' cr'
