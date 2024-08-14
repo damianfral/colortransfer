7e67258b593fca52f1768a4e5967ef9eb99e1ad6
damianfral <huevofritopamojarpan@gmail.com>
2024-08-14 19:53:45 +0000

    Use method (Int) instead of histogram (Bool).

--- src/ColorTransfer/CLI.hs
+++ src/ColorTransfer/CLI.hs
@@ -18,7 +18,7 @@ data Opts w = Opts
   { input :: w ::: String <?> "the image to be used as the color source",
     reference :: w ::: String <?> "the image we want to transform",
     output :: w ::: String <?> "the output image",
-    histogram :: w ::: Bool <?> "use simple histogram matching"
+    method :: w ::: Int <!> "2" <?> "1 histogram matching, 2 ellipsoid transformation"
   }
   deriving (Generic)
 
@@ -28,8 +28,9 @@ runColorTransfer :: Opts Unwrapped -> IO ()
 runColorTransfer (Opts {..}) = do
   inputImg <- P.readImage input
   referenceImg <- P.readImage reference
+  let transfer = if method == 1 then HM.transferColor else ET.transferColor
   let result =
-        (if histogram then HM.transferColor else ET.transferColor)
+        transfer
           <$> (dynamicImagetoYCbCr <$> inputImg)
           <*> (dynamicImagetoYCbCr <$> referenceImg)
 
