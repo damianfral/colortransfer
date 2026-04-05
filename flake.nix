330b8bf6787a6bd7e20afb6af36dffd0e82de98b
damianfral <huevofritopamojarpan@gmail.com>
2026-04-05 17:18:31 +0000

    Use nixpkgs/release-25.11.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A tool to transfer color between images.";
 
   inputs = {
-    nixpkgs = {url = "github:NixOS/nixpkgs/25.05";};
+    nixpkgs = {url = "github:NixOS/nixpkgs/25.11";};
     flake-utils = {url = "github:numtide/flake-utils";};
     nix-filter.url = "github:numtide/nix-filter";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
